7d824bae0fe8d87fc20d298d68a016b7772fa41e
gchanakya7-dev <gchanakya7@gmail.com>
2025-12-22 10:05:41 +0000

    Basci

--- src/config/sheets.ts
+++ src/config/sheets.ts
@@ -1,5 +1,5 @@
 export const SHEET_IDS = {
-  basicInfo: "1nFYXIts878Su0KgUmQjXTDedjHilToASZ0WQs14eMSw",
+  basicInfo: "12ldWoK9YamCXX3jik9CVxlX0mFFsjAhB_kB2DF1CMwQ",
   qualifications: "1kq9TfO8WIFhwN9C66GnWrczi7qgny1VnSH21ERUO-Ck",
   teaching: "13xcDqjb3H3pi7YxL1xagcitBSKM819YVQygLxA2imao",
   courses: "1Io_jlOW2DCYUxWAx-ID9DMEKj6spVxC39qHGE9Yr0uI",
